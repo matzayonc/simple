04449489f9651b0ad73e63714a47c135caa30177
Mateusz Zając <matzayonc>
2021-06-07 19:12:31 +0200

    split tests and added comments

--- tests/simple.spec.ts
+++ tests/simple.spec.ts
@@ -1,18 +1,17 @@
-import * as anchor from "@project-serum/anchor"
-import { Program, BN, Provider } from "@project-serum/anchor"
-import { TokenInstructions } from "@project-serum/serum"
+import * as anchor from '@project-serum/anchor'
+import { Program, BN, Provider } from '@project-serum/anchor'
+import { TokenInstructions } from '@project-serum/serum'
 import {
-	PublicKey,
-	Keypair,
-	Connection,
-	Transaction,
-	sendAndConfirmRawTransaction,
-	Account,
-} from "@solana/web3.js"
-import { Token, u64, TOKEN_PROGRAM_ID } from "@solana/spl-token"
-import { assert } from "chai"
+  PublicKey,
+  Keypair,
+  Connection,
+  Transaction,
+  sendAndConfirmRawTransaction
+} from '@solana/web3.js'
+import { Token, u64, TOKEN_PROGRAM_ID } from '@solana/spl-token'
+import { assert } from 'chai'
 
-const SEED = Buffer.from("Synthetify")
+const SEED = Buffer.from('Synthetify')
 export const mainProgram = anchor.workspace.Simple as Program
 let nonce: number
 
@@ -24,129 +23,106 @@ let programAuthority: PublicKey
 export let someToken: Token
 
 before(async () => {
-	anchor.setProvider(anchor.Provider.env())
+  anchor.setProvider(anchor.Provider.env())
 
-	const [_programAuthority, _nonce] =
-		await anchor.web3.PublicKey.findProgramAddress(
-			[SEED],
-			mainProgram.programId
-		)
-	programAuthority = _programAuthority
-	nonce = _nonce
+  const [_programAuthority, _nonce] = await anchor.web3.PublicKey.findProgramAddress(
+    [SEED],
+    mainProgram.programId
+  )
+  programAuthority = _programAuthority
+  nonce = _nonce
 
-	someToken = await createToken(connection, wallet, programAuthority)
+  someToken = await createToken(connection, wallet, programAuthority)
 })
-
-async function createToken(
-	connection: Connection,
-	payer: Keypair,
-	mintAuthority: PublicKey
-) {
-	const token = await Token.createMint(
-		connection,
-		payer,
-		mintAuthority,
-		null,
-		4,
-		TokenInstructions.TOKEN_PROGRAM_ID
-	)
-	return token
-}
 
 let owner = Keypair.generate()
 let staking: PublicKey
 let tokens: PublicKey
 
-describe("Simple", () => {
-	it("Mint", async () => {
-		//creating accounts
-		staking = await someToken.createAccount(programAuthority)
-		tokens = await someToken.createAccount(owner.publicKey)
+describe('Simple', () => {
+  it('Mint', async () => {
+    //creating accounts
+    staking = await someToken.createAccount(programAuthority)
+    tokens = await someToken.createAccount(owner.publicKey)
+    //minting tokens
+    await mainProgram.rpc.mintTokens(nonce, {
+      accounts: {
+        mint: someToken.publicKey,
+        auth: programAuthority,
+        owner: owner.publicKey,
+        tokens,
+        tokenProgram: TokenInstructions.TOKEN_PROGRAM_ID,
+        rent: anchor.web3.SYSVAR_RENT_PUBKEY
+      },
+      signers: [owner]
+    })
 
-		await mainProgram.rpc.mintTokens(nonce, {
-			accounts: {
-				mint: someToken.publicKey,
-				auth: programAuthority,
-				owner: owner.publicKey,
-				tokens,
-				tokenProgram: TokenInstructions.TOKEN_PROGRAM_ID,
-				rent: anchor.web3.SYSVAR_RENT_PUBKEY,
-			},
-			signers: [owner],
-		})
+    //checking balance
+    assert.ok((await someToken.getAccountInfo(tokens)).amount.eq(to64(50)))
+  })
 
-		assert.ok((await someToken.getAccountInfo(tokens)).amount.eq(to64(50)))
+  it('Deposit', async () => {
+    const amountToSend = to64(42)
+    //creating instructions
+    const depositIx = await depositInstruction(amountToSend, staking, tokens, owner.publicKey)
 
-		const amountToSend = to64(42)
+    const approveIx = Token.createApproveInstruction(
+      someToken.programId,
+      tokens,
+      programAuthority,
+      owner.publicKey,
+      [],
+      amountToSend
+    )
 
-		const depositIx = await depositInstruction({
-			amount: amountToSend,
-			collateralAccount: staking,
-			userCollateralAccount: tokens,
-			owner: owner.publicKey,
-		})
+    //sending Transaction
+    await signAndSend(new Transaction().add(approveIx).add(depositIx), [wallet, owner], connection)
 
-		const approveIx = Token.createApproveInstruction(
-			someToken.programId,
-			tokens,
-			programAuthority,
-			owner.publicKey,
-			[],
-			amountToSend
-		)
-
-		await signAndSend(
-			new Transaction().add(approveIx).add(depositIx),
-			[wallet, owner],
-			connection
-		)
-
-		assert.ok((await someToken.getAccountInfo(tokens)).amount.eq(to64(8)))
-		assert.ok((await someToken.getAccountInfo(staking)).amount.eq(to64(42)))
-	})
+    //checking balances
+    assert.ok((await someToken.getAccountInfo(tokens)).amount.eq(to64(8)))
+    assert.ok((await someToken.getAccountInfo(staking)).amount.eq(to64(42)))
+  })
 })
 
 function to64(amount) {
-	return new u64(amount.toString())
+  return new u64(amount.toString())
 }
 
-async function signAndSend(tx, signers, connection, opts?) {
-	tx.setSigners(...signers.map(s => s.publicKey))
-	const blockhash = await connection.getRecentBlockhash(
-		(opts === null || opts === void 0 ? void 0 : opts.commitment) ||
-			Provider.defaultOptions().commitment
-	)
+async function signAndSend(tx, signers, connection) {
+  tx.setSigners(...signers.map((s) => s.publicKey))
+  const blockhash = await connection.getRecentBlockhash(Provider.defaultOptions().commitment)
 
-	tx.recentBlockhash = blockhash.blockhash
-	tx.partialSign(...signers)
-	const rawTx = tx.serialize()
-	return await sendAndConfirmRawTransaction(
-		connection,
-		rawTx,
-		Provider.defaultOptions()
-	)
+  tx.recentBlockhash = blockhash.blockhash
+  tx.partialSign(...signers)
+  const rawTx = tx.serialize()
+  return await sendAndConfirmRawTransaction(connection, rawTx, Provider.defaultOptions())
 }
 
-export interface DepositInstruction {
-	collateralAccount: PublicKey
-	userCollateralAccount: PublicKey
-	owner: PublicKey
-	amount: BN
+async function depositInstruction(
+  amount: BN,
+  collateralAccount: PublicKey,
+  userCollateralAccount: PublicKey,
+  owner: PublicKey
+) {
+  return await mainProgram.instruction.deposit(amount, nonce, {
+    accounts: {
+      owner: owner,
+      collateralAccount: collateralAccount,
+      userCollateralAccount: userCollateralAccount,
+      tokenProgram: TOKEN_PROGRAM_ID,
+      exchangeAuthority: programAuthority
+    }
+  })
 }
 
-async function depositInstruction({
-	amount,
-	collateralAccount,
-	userCollateralAccount,
-	owner,
-}: DepositInstruction) {
-	return await mainProgram.instruction.deposit(amount, nonce, {
-		accounts: {
-			owner: owner,
-			collateralAccount: collateralAccount,
-			userCollateralAccount: userCollateralAccount,
-			tokenProgram: TOKEN_PROGRAM_ID,
-			exchangeAuthority: programAuthority,
-		},
-	})
+async function createToken(connection: Connection, payer: Keypair, mintAuthority: PublicKey) {
+  const token = await Token.createMint(
+    connection,
+    payer,
+    mintAuthority,
+    null,
+    4,
+    TokenInstructions.TOKEN_PROGRAM_ID
+  )
+  return token
 }
